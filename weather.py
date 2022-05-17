a8bc41ab1ee1451711e74b67975309ee2cc35a12
jenguyenanhnhu <102585311+jenguyenanhnhu@users.noreply.github.com>
2022-05-17 17:39:17 +1000

    Test 1, 2, and 3 successful
    Somehow, by some miracle, I converted temperature, iso date, and mean for weather data.

--- weather.py
+++ weather.py
@@ -1,74 +1,90 @@
-import csv
 from datetime import datetime
+from itertools import tee
+import statistics
+import csv
 
-DEGREE_SYBMOL = u"\N{DEGREE SIGN}C"
-
-
-def format_temperature(temp):
-    """Takes a temperature and returns it in string format with the degrees
-        and celcius symbols.
-
-    Args:
-        temp: A string representing a temperature.
-    Returns:
-        A string contain the temperature and "degrees celcius."
-    """
-    return f"{temp}{DEGREE_SYBMOL}"
+DEGREE_SYMBOL = u"\N{DEGREE SIGN}C"
 
 
 def convert_date(iso_string):
-    """Converts and ISO formatted date into a human readable format.
+    return datetime.fromisoformat(iso_string).strftime("%A %d %B %Y")
+# print(convert_date("2021-07-02T07:00:00+08:00"))
+    # """Converts an ISO formatted date into a human readable format.
+    # Args:
+    #     iso_string: An ISO date string.
+    # Returns:
+    #     A date formatted like: Weekday Date Month Year e.g. Tuesday 06 July 2021
+    # """
+    # pass
 
-    Args:
-        iso_string: An ISO date string..
-    Returns:
-        A date formatted like: Weekday Date Month Year e.g. Tuesday 06 July 2021
-    """
-    pass
+
+def format_temperature(temp):
+    return f"{temp}{DEGREE_SYMBOL}"
+# print(format_temperature(3))
+    # """Takes a temperature and returns it in string format with the degrees
+    #     and Celcius symbols.
+    # Args:
+    #     temp: A string representing a temperature.
+    # Returns:
+    #     A string contain the temperature and "degrees Celsius."
+    # """
 
 
-def convert_f_to_c(temp_in_farenheit):
-    """Converts an temperature from farenheit to celcius.
-
-    Args:
-        temp_in_farenheit: float representing a temperature.
-    Returns:
-        A float representing a temperature in degrees celcius, rounded to 1dp.
-    """
-    pass
-
+def convert_f_to_c(temp_in_fahrenheit):
+    if isinstance(temp_in_fahrenheit, float):
+        return round((int(temp_in_fahrenheit) - 32)/1.8)
+    else:
+        return round((int(temp_in_fahrenheit) - 32)/1.8, 1)
+# print(convert_f_to_c(0))
+    # """Converts an temperature from fahrenheit to Celsius.
+    # Args:
+    #     temp_in_fahrenheit: float representing a temperature.
+    # Returns:
+    #     A float representing a temperature in degrees celsius, rounded to 1dp.
+    # """
+    # pass
 
 def calculate_mean(weather_data):
-    """Calculates the mean value from a list of numbers.
-
-    Args:
-        weather_data: a list of numbers.
-    Returns:
-        A float representing the mean value.
-    """
-    pass
+    return statistics.mean(map(float, weather_data))
+# print(calculate_mean([49, 57, 56, 55, 53]))
+    # """Calculates the mean value from a list of numbers.
+    # Args:
+    #     weather_data: a list of numbers.
+    # Returns:
+    #     A float representing the mean value.
+    # """
+    # pass
 
 
 def load_data_from_csv(csv_file):
-    """Reads a csv file and stores the data in a list.
-
-    Args:
-        csv_file: a string representing the file path to a csv file.
-    Returns:
-        A list of lists, where each sublist is a (non-empty) line in the csv file.
-    """
-    pass
+    with open(csv_file) as csv_file:
+        reader = csv.reader(csv_file)
+        next(csv_file)
+        for line in reader:
+            return line
+# print(load_data_from_csv("tests/data/example_one.csv"))
+    # """Reads a csv file and stores the data in a list.
+    # Args:
+    #     csv_file: a string representing the file path to a csv file.
+    # Returns:
+    #     A list of lists, where each sublist is a (non-empty) line in the csv file.
+    # """
+    # pass
 
 
 def find_min(weather_data):
-    """Calculates the minimum value in a list of numbers.
-
-    Args:
-        weather_data: A list of numbers.
-    Returns:
-        The minium value and it's position in the list.
-    """
-    pass
+    if min(x for x in weather_data if weather_data.count(x) > 1):
+        return min(map(float, weather_data))
+    else:
+        return min(map(float, weather_data)), weather_data.index(min(map(float, weather_data)))
+print(find_min([3, 5, 2, 9, 2]))
+    # """Calculates the minimum value in a list of numbers.
+    # Args:
+    #     weather_data: A list of numbers.
+    # Returns:
+    #     The minium value and it's position in the list.
+    # """
+    # pass
 
 
 def find_max(weather_data):
