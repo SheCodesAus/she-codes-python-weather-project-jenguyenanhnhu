93c7304ee799982f1bf36bce96f62d067c75c02b
jenguyenanhnhu <102585311+jenguyenanhnhu@users.noreply.github.com>
2022-05-22 23:16:35 +1000

    Got all my tests to work!
    220522 - with less than an hour to the deadline.

--- weather.py
+++ weather.py
@@ -45,7 +45,7 @@ def convert_f_to_c(temp_in_fahrenheit):
     # pass
 
 def calculate_mean(weather_data):
-    return statistics.mean(map(float(weather_data)))
+    return statistics.mean(map(float,weather_data))
 # print(calculate_mean([49, 57, 56, 55, 53]))
     # """Calculates the mean value from a list of numbers.
     # Args:
@@ -86,14 +86,13 @@ def load_data_from_csv(csv_file):
 #     #     return min(map(float, weather_data)), weather_data.index(min(map(float, weather_data)))
 # print(find_min([3, 5, 0, 9, 0]))
 
-
 def find_min(weather_data):
     if weather_data:
         min_val = min(weather_data)
         for i in range(len(weather_data)):
             if weather_data[i] == min_val:
-                idx = i
-        return float(min_val), idx
+                index = i
+        return float(min_val), index
     else:
         return ()
     # """Calculates the minimum value in a list of numbers.
@@ -104,13 +103,14 @@ def find_min(weather_data):
     # """
     # pass
 
+
 def find_max(weather_data):
     if weather_data:
         max_val = max(weather_data)
         for i in range(len(weather_data)):
             if weather_data[i] == max_val:
-                idx = i
-        return float(max_val), idx
+                index = i
+        return float(max_val), index
     else:
         return ()
     # """Calculates the maximum value in a list of numbers.
@@ -123,20 +123,59 @@ def find_max(weather_data):
 
 
 def generate_summary(weather_data):
-    """Outputs a summary for the given weather data.
-    Args:
-        weather_data: A list of lists, where each sublist represents a day of weather data.
-    Returns:
-        A string containing the summary information.
-    """
-    pass
+    n = len(weather_data)
+    date_list = []
+    min_list = []
+    max_list = []
+    for value in weather_data:
+        date_list.append(value[0])
+        min_list.append(value[1])
+        max_list.append(value[2])
+    min_value = min(min_list)
+    min_temp = round((min_value - 32)/1.8, 1)
+    for a in range(len(min_list)):
+        if min_list[a]==min_value:
+            min_date = datetime.fromisoformat(date_list[a]).strftime("%A %d %B %Y")
+    max_value = max(max_list)
+    max_temp = round((max_value - 32)/1.8, 1)
+    for b in range(len(max_list)):
+        if max_list[b]==max_value:
+            max_date = datetime.fromisoformat(date_list[b]).strftime("%A %d %B %Y")
+    min_mean_value = statistics.mean(map(float,min_list))
+    min_mean = round((min_mean_value - 32)/1.8, 1)
+    max_mean_value = statistics.mean(map(float,max_list))
+    max_mean = round((max_mean_value - 32)/1.8, 1)
+    return f"{n} Day Overview\n  The lowest temperature will be {min_temp}{DEGREE_SYMBOL}, and will occur on {min_date}.\n  The highest temperature will be {max_temp}{DEGREE_SYMBOL}, and will occur on {max_date}.\n  The average low this week is {min_mean}{DEGREE_SYMBOL}.\n  The average high this week is {max_mean}{DEGREE_SYMBOL}.\n"
+print(generate_summary([["2020-06-19T07:00:00+08:00", 47, 46],
+    ["2020-06-20T07:00:00+08:00", 51, 67],
+    ["2020-06-21T07:00:00+08:00", 58, 72],
+    ["2020-06-22T07:00:00+08:00", 59, 71],
+    ["2020-06-23T07:00:00+08:00", 52, 71],
+    ["2020-06-24T07:00:00+08:00", 52, 67],
+    ["2020-06-25T07:00:00+08:00", 48, 66],
+    ["2020-06-26T07:00:00+08:00", 53, 66]]
+))
+    # """Outputs a summary for the given weather data.
+    # Args:
+    #     weather_data: A list of lists, where each sublist represents a day of weather data.
+    # Returns:
+    #     A string containing the summary information.
+    # """
+    # pass
 
 
 def generate_daily_summary(weather_data):
-    """Outputs a daily summary for the given weather data.
-    Args:
-        weather_data: A list of lists, where each sublist represents a day of weather data.
-    Returns:
-        A string containing the summary information.
-    """
-    pass
+    result = ""
+    for value in weather_data:
+        date = datetime.fromisoformat(value[0]).strftime("%A %d %B %Y")
+        min_temp = round((int(value[1]) - 32)/1.8, 1)
+        max_temp = round((int(value[2]) - 32)/1.8, 1)
+        result += f"---- {date} ----\n  Minimum Temperature: {min_temp}{DEGREE_SYMBOL}\n  Maximum Temperature: {max_temp}{DEGREE_SYMBOL}\n\n"
+    return result
+    # """Outputs a daily summary for the given weather data.
+    # Args:
+    #     weather_data: A list of lists, where each sublist represents a day of weather data.
+    # Returns:
+    #     A string containing the summary information.
+    # """
+    # pass
